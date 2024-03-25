c4ca5388d043084ac1dd0e48bf7d7748558ce431
Wang HoEun <hoeunwang@gmail.com>
2024-03-25 19:48:35 +0900

    chore: 공통 응답 객체 생성

---
+++ src/main/java/org/kau/kkoolbeeServer/global/common/dto/ApiResponse.java
@@ -0,0 +1,51 @@
+package org.kau.kkoolbeeServer.global.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.kau.kkoolbeeServer.global.common.enums.ErrorType;
+import org.kau.kkoolbeeServer.global.common.enums.SuccessType;
+
+@Getter
+@JsonPropertyOrder({"code", "message", "data"})
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private final int code; // 상태 코드
+    private final String message; // 회원 조회가 정상 처리되었습니다!
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data; // 객체가 실제로 들어감
+
+    public static ApiResponse<?> success(SuccessType successType) {
+        return new ApiResponse<>(successType.getHttpStatusCode(), successType.getMessage());
+    }
+
+    public static <T> ApiResponse<T> success(SuccessType successType, T data) {
+        return new ApiResponse<T>(successType.getHttpStatusCode(), successType.getMessage(), data);
+    }
+
+    public static ApiResponse<?> error(ErrorType errorType) {
+        return new ApiResponse<>(errorType.getHttpStatusCode(), errorType.getMessage());
+    }
+
+    public static ApiResponse<?> error(ErrorType errorType, String message) {
+        return new ApiResponse<>(errorType.getHttpStatusCode(), message);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorType errorType, String message, T data) {
+        return new ApiResponse<>(errorType.getHttpStatusCode(), message, data);
+    }
+
+    public static <T> ApiResponse<Exception> error(ErrorType errorType, Exception e) {
+        return new ApiResponse<>(errorType.getHttpStatusCode(), errorType.getMessage(), e);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorType errorType, T data) {
+        return new ApiResponse<>(errorType.getHttpStatusCode(), errorType.getMessage(), data);
+    }
+}
