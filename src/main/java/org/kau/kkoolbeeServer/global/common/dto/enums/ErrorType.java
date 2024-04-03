ec9f632421e7211668cf4818f20bb9e57ddb2ca0
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2024-04-03 23:36:46 +0900

    Develop에서 main 최신화 (#12)
    * chore: 환경변수 세팅
    
    * chore: aws secret manager 설정
    
    * [chore] CI/CD settings (#13)
    
    * chore: common 폴더 구조 변경
    
    * chore: 환경변수 파일 분리 (local/dev)
    
    * feat: 서버 profile 확인용 api 추가
    
    * chore: 무중단 배포시 실행할 스크립트 작성
    
    * chore: Github Actions 스크립트 작성 (임시 trigger 설정)
    
    * chore: CI/CD trigger 재설정
    
    * chore: 인덴트 오류 수정
    
    * chore: CodeDeploy 옵션 문자열 오류 수정
    
    * chore: Spring Security 및 Redis 임시 주석 처리
    
    * chore: trigger 시점 develop 으로 변경
    
    * chore: profile 조회 엔드포인트 수정
    
    * style: CD 브랜치 trigger 시점 임시 변경
    
    * fix: trigger 시점 develop 으로 원상 복귀
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- src/main/java/org/kau/kkoolbeeServer/global/common/dto/enums/ErrorType.java
+++ src/main/java/org/kau/kkoolbeeServer/global/common/dto/enums/ErrorType.java
@@ -1,4 +1,4 @@
-package org.kau.kkoolbeeServer.global.common.enums;
+package org.kau.kkoolbeeServer.global.common.dto.enums;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -7,12 +7,17 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public enum SuccessType {
+public enum ErrorType {
 
     /**
-     * 200 OK
+     * 400 BAD REQUEST
      */
-    PROCESS_SUCCESS(HttpStatus.OK, "OK"),
+    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+
+    /**
+     * 500 INTERNAL SERVER ERROR
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다."),
     ;
 
     private final HttpStatus httpStatus;
--- src/main/java/org/kau/kkoolbeeServer/global/common/dto/enums/SuccessType.java
+++ src/main/java/org/kau/kkoolbeeServer/global/common/dto/enums/SuccessType.java
@@ -1,4 +1,4 @@
-package org.kau.kkoolbeeServer.global.common.enums;
+package org.kau.kkoolbeeServer.global.common.dto.enums;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -7,17 +7,12 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public enum ErrorType {
+public enum SuccessType {
 
     /**
-     * 400 BAD REQUEST
+     * 200 OK
      */
-    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
-
-    /**
-     * 500 INTERNAL SERVER ERROR
-     */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다."),
+    PROCESS_SUCCESS(HttpStatus.OK, "OK"),
     ;
 
     private final HttpStatus httpStatus;
