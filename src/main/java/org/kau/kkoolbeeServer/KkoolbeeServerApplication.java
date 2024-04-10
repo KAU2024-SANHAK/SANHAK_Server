e32370f7185a8d23d134f059fc3efd3d6480da45
jwaminseok <darth0324@naver.com>
2024-04-10 11:58:16 +0900

    feat/14-entity develop브랜치에 merge

--- src/main/java/org/kau/kkoolbeeServer/KkoolbeeServerApplication.java
+++ src/main/java/org/kau/kkoolbeeServer/KkoolbeeServerApplication.java
@@ -1,5 +1,6 @@
 package org.kau.kkoolbeeServer;
 
+import org.kau.kkoolbeeServer.domain.diary.Diary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/org/kau/kkoolbeeServer/domain/advice/Advice.java
@@ -0,0 +1,15 @@
+package org.kau.kkoolbeeServer.domain.advice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Advice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String content;
+}
