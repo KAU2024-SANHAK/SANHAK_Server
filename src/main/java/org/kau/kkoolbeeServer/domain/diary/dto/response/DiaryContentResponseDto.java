44f8326c9306f9b75f34015ebd81bc2abefceadd
jwaminseok <darth0324@naver.com>
2024-04-12 18:23:33 +0900

    json필드  이름변경

--- src/main/java/org/kau/kkoolbeeServer/domain/diary/dto/response/DiaryContentResponseDto.java
+++ src/main/java/org/kau/kkoolbeeServer/domain/diary/dto/response/DiaryContentResponseDto.java
@@ -1,5 +1,6 @@
 package org.kau.kkoolbeeServer.domain.diary.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.kau.kkoolbeeServer.domain.advice.dto.AdviceResponseDto;
@@ -7,6 +8,7 @@ import org.kau.kkoolbeeServer.domain.advice.dto.AdviceResponseDto;
 @Getter
 @AllArgsConstructor
 public class DiaryContentResponseDto {
+    @JsonProperty("diary_content")
     private String diaryContent;
     private AdviceResponseDto advice;
     private String feeling;
