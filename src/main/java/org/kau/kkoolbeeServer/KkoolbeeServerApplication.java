4ff729f359cd61a26e85a8a412cae745d6abc4df
jwaminseok <darth0324@naver.com>
2024-04-14 15:50:39 +0900

    애플리케이션 timezone을 utc로 설정하여 임시해결

--- src/main/java/org/kau/kkoolbeeServer/KkoolbeeServerApplication.java
+++ src/main/java/org/kau/kkoolbeeServer/KkoolbeeServerApplication.java
@@ -1,5 +1,6 @@
 package org.kau.kkoolbeeServer;
 
+import jakarta.annotation.PostConstruct;
 import org.kau.kkoolbeeServer.domain.diary.Diary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +12,11 @@ public class KkoolbeeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KkoolbeeServerApplication.class, args);
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		TimeZone tz = TimeZone.getDefault();
 		System.out.println("현재 시간대: " + tz.getID());
 
 	}
 
 }
+
