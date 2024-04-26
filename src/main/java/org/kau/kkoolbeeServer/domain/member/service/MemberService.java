48e8a21bef251a4644028ed7f8fc960fedb967e9
Wang HoEun <hoeunwang@gmail.com>
2024-04-27 02:47:05 +0900

    feat: 카카오 로그인 구현+s3서버 세팅

---
+++ src/main/java/org/kau/kkoolbeeServer/domain/member/service/MemberService.java
@@ -0,0 +1,93 @@
+package org.kau.kkoolbeeServer.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kau.kkoolbeeServer.domain.member.Member;
+import org.kau.kkoolbeeServer.domain.member.dto.response.MemberLoginResponseDto;
+import org.kau.kkoolbeeServer.domain.member.repository.MemberRepository;
+import org.kau.kkoolbeeServer.global.auth.fegin.kakao.KakaoLoginService;
+import org.kau.kkoolbeeServer.global.auth.jwt.JwtProvider;
+import org.kau.kkoolbeeServer.global.auth.jwt.TokenDto;
+import org.kau.kkoolbeeServer.global.auth.security.UserAuthentication;
+import org.kau.kkoolbeeServer.global.common.exception.model.CustomException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.kau.kkoolbeeServer.global.common.dto.enums.ErrorType.INVALID_TOKEN_HEADER_ERROR;
+import static org.kau.kkoolbeeServer.global.common.dto.enums.ErrorType.NOT_FOUND_MEMBER_ERROR;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final JwtProvider jwtProvider;
+    private final KakaoLoginService kakaoLoginService;
+
+    private static String parseTokenString(String tokenString) {
+        String[] strings = tokenString.split(" ");
+        if (strings.length != 2) {
+            throw new CustomException(INVALID_TOKEN_HEADER_ERROR);
+        }
+        return strings[1];
+    }
+
+    @Transactional
+    public MemberLoginResponseDto login(String socialAccessToken) {
+
+        socialAccessToken = parseTokenString(socialAccessToken);
+
+        String kakaoId = kakaoLoginService.getKakaoId(socialAccessToken);
+
+        boolean isRegistered = isUserByKakaoId(kakaoId);
+        if (!isRegistered) {
+            Member member = Member.builder()
+                .kakaoId(kakaoId).build();
+
+            memberRepository.save(member);
+        }
+
+        Member loginMember = getUserBySocialAndSocialId(kakaoId);
+        // 카카오 로그인은 정보 더 많이 받아올 수 있으므로 추가 설정
+        kakaoLoginService.setKakaoInfo(loginMember, socialAccessToken);
+
+        TokenDto tokenDto = jwtProvider.issueToken(
+            new UserAuthentication(loginMember.getId(), null, null));
+
+        return MemberLoginResponseDto.of(loginMember, tokenDto);
+    }
+
+    @Transactional
+    public TokenDto reissueToken(String refreshToken) {
+
+        refreshToken = parseTokenString(refreshToken);
+
+        Long memberId = jwtProvider.validateRefreshToken(refreshToken);
+        validateMemberId(memberId);  // memberId가 DB에 저장된 유효한 값인지 검사
+
+        jwtProvider.deleteRefreshToken(memberId);
+        return jwtProvider.issueToken(new UserAuthentication(memberId, null, null));
+    }
+
+    @Transactional
+    public void logout(Long memberId) {
+        jwtProvider.deleteRefreshToken(memberId);
+    }
+
+    private void validateMemberId(Long memberId) {
+        if (!memberRepository.existsById(memberId)) {
+            throw new CustomException(NOT_FOUND_MEMBER_ERROR);
+        }
+    }
+
+    private Member getUserBySocialAndSocialId(String kakaoId) {
+        return memberRepository.findByKakaoId(kakaoId)
+            .orElseThrow(() -> new CustomException(NOT_FOUND_MEMBER_ERROR));
+    }
+
+    private boolean isUserByKakaoId(String kakaoId) {
+        return memberRepository.existsByKakaoId(kakaoId);
+    }
+}
--- src/main/java/org/kau/kkoolbeeServer/global/common/dto/enums/ErrorType.java
+++ src/main/java/org/kau/kkoolbeeServer/global/common/dto/enums/ErrorType.java
@@ -12,7 +12,35 @@ public enum ErrorType {
     /**
      * 400 BAD REQUEST
      */
-    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+    // 표준 오류
+    REQUEST_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INVALID_TYPE_ERROR(HttpStatus.BAD_REQUEST, "잘못된 타입이 입력되었습니다."),
+    INVALID_MISSING_HEADER_ERROR(HttpStatus.BAD_REQUEST, "요청에 필요한 헤더값이 존재하지 않습니다."),
+    INVALID_HTTP_REQUEST_ERROR(HttpStatus.BAD_REQUEST, "요청 형식이 허용된 형식과 다릅니다."),
+    INVALID_HTTP_METHOD_ERROR(HttpStatus.BAD_REQUEST, "지원되지 않는 HTTP method 요청입니다."),
+    INVALID_TOKEN_HEADER_ERROR(HttpStatus.BAD_REQUEST, "토큰 헤더값의 형식이 잘못되었습니다."),
+    INVALID_CODE_HEADER_ERROR(HttpStatus.BAD_REQUEST, "code 헤더값의 형식이 잘못되었습니다."),
+    // 인증 관련 오류
+    EMPTY_PRINCIPLE_ERROR(HttpStatus.BAD_REQUEST, "Principle 객체가 없습니다. (null)"),
+
+    /**
+     * 401 UNAUTHORIZED
+     */
+    INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않는 JWT 토큰입니다."),
+    EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
+    UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT 토큰입니다."),
+    EMPTY_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "JWT 토큰이 존재하지 않습니다."),
+    INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "잘못된 JWT 서명입니다."),
+    UNKNOWN_JWT_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 JWT 토큰 오류가 발생했습니다."),
+
+    INVALID_SOCIAL_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 소셜 엑세스 토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다, 다시 로그인을 해주세요."),
+
+    /**
+     * 404 NOT FOUND
+     */
+    NOT_FOUND_MEMBER_ERROR(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    NOT_FOUND_REFRESH_TOKEN_ERROR(HttpStatus.NOT_FOUND, "존재하지 않는 리프레시 토큰입니다."),
 
     /**
      * 500 INTERNAL SERVER ERROR
