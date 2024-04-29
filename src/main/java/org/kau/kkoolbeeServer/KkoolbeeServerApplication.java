e2d716a82df3dddcd9c05b57898b13ab7c6b9314
jwaminseok <darth0324@naver.com>
2024-04-29 13:42:59 +0900

    develop pull 마무리

--- src/main/java/org/kau/kkoolbeeServer/KkoolbeeServerApplication.java
+++ src/main/java/org/kau/kkoolbeeServer/KkoolbeeServerApplication.java
@@ -1,22 +1,24 @@
 package org.kau.kkoolbeeServer;
 
-import jakarta.annotation.PostConstruct;
-import org.kau.kkoolbeeServer.domain.diary.Diary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+@EnableFeignClients
 public class KkoolbeeServerApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(KkoolbeeServerApplication.class, args);
 		SpringApplication.run(KkoolbeeServerApplication.class, args);
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		TimeZone tz = TimeZone.getDefault();
 		System.out.println("현재 시간대: " + tz.getID());
 
+
 	}
 
 }
-
