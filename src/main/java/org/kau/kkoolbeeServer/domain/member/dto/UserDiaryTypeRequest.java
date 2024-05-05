5591fa00852d580a255d8dd4d0824f912e522454
jwaminseok <darth0324@naver.com>
2024-05-05 22:01:05 +0900

    requestHeader를 사용한 코드수정 및 diarytyperequestDto추가

---
+++ src/main/java/org/kau/kkoolbeeServer/domain/member/dto/UserDiaryTypeRequest.java
@@ -0,0 +1,16 @@
+package org.kau.kkoolbeeServer.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDiaryTypeRequest {
+
+    private String userDiaryType;
+
+}
