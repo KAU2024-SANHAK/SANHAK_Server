e62b5508fa2f8cddab9c6c6fade4544bc3330f39
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-05-10 00:35:24 +0900

    refactor:content api에서 advice와 feeling값 null값허용 (#59)

--- src/main/java/org/kau/kkoolbeeServer/domain/advice/dto/AdviceResponseDto.java
+++ src/main/java/org/kau/kkoolbeeServer/domain/advice/dto/AdviceResponseDto.java
@@ -3,10 +3,19 @@ package org.kau.kkoolbeeServer.domain.advice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.kau.kkoolbeeServer.domain.advice.Advice;
 
 @Getter
 @AllArgsConstructor
 public class AdviceResponseDto {
     private String spicy;
     private String kind;
+
+    public static AdviceResponseDto fromAdviceOrNull(Advice advice) {
+        if (advice == null) {
+            return new AdviceResponseDto(null, null);
+        } else {
+            return new AdviceResponseDto(advice.getSpicy_advice(), advice.getKind_advice());
+        }
+    }  //advice가 null이어도 응답메시지를 보내야하므로
 }
