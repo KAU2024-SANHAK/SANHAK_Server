93f26f5a9ba1d8e8d5609966bf7a1a0e80a86071
jwaminseok <109052897+jms0324@users.noreply.github.com>
2024-05-15 00:42:39 +0900

    feat: 수정 api구현 (#76)

---
+++ src/main/java/org/kau/kkoolbeeServer/domain/diary/dto/response/UpdateDiaryResponseDto.java
@@ -0,0 +1,15 @@
+package org.kau.kkoolbeeServer.domain.diary.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UpdateDiaryResponseDto {
+    private Long diaryId;
+    private String diaryContent;
+    private String diaryTitle;
+    private String imageurl;
+}
