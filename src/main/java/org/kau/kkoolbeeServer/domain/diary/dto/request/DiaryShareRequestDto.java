e4bad5dd0bc1c53bf8a344037e521800dea21fb9
jwaminseok <darth0324@naver.com>
2024-05-26 15:33:51 +0900

    feag:카카오톡공유하기api구현

---
+++ src/main/java/org/kau/kkoolbeeServer/domain/diary/dto/request/DiaryShareRequestDto.java
@@ -0,0 +1,11 @@
+package org.kau.kkoolbeeServer.domain.diary.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DiaryShareRequestDto {
+
+    private Long diaryId;
+}
---
+++ src/main/java/org/kau/kkoolbeeServer/domain/diary/dto/response/DiaryShareResponseDto.java
@@ -0,0 +1,22 @@
+package org.kau.kkoolbeeServer.domain.diary.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.kau.kkoolbeeServer.domain.advice.dto.AdviceResponseDto;
+import org.kau.kkoolbeeServer.domain.diary.Feeling;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiaryShareResponseDto {
+    private Long diaryId;
+    private String diaryContent;
+    private String diaryTitle;
+    private String imageUrl;
+    private String userName;
+    private AdviceResponseDto advice;
+    private String feeling;
+}
